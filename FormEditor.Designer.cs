4fe5ae952f50a132b8f84f0ada00543b80f12737
Retinug <Retinug@gmail.com>
2020-10-30 13:41:10 +0300

    Getting table names

--- FormEditor.Designer.cs
+++ FormEditor.Designer.cs
@@ -28,13 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox_SelectTable = new System.Windows.Forms.ComboBox();
+            this.label_SelectTable = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(199, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(319, 350);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(178, 377);
+            this.panel1.TabIndex = 1;
+            // 
+            // comboBox_SelectTable
+            // 
+            this.comboBox_SelectTable.FormattingEnabled = true;
+            this.comboBox_SelectTable.Location = new System.Drawing.Point(288, 368);
+            this.comboBox_SelectTable.Name = "comboBox_SelectTable";
+            this.comboBox_SelectTable.Size = new System.Drawing.Size(230, 21);
+            this.comboBox_SelectTable.TabIndex = 2;
+            // 
+            // label_SelectTable
+            // 
+            this.label_SelectTable.AutoSize = true;
+            this.label_SelectTable.Location = new System.Drawing.Point(196, 371);
+            this.label_SelectTable.Name = "label_SelectTable";
+            this.label_SelectTable.Size = new System.Drawing.Size(86, 13);
+            this.label_SelectTable.TabIndex = 3;
+            this.label_SelectTable.Text = "Выбор таблицы";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(530, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // FormEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(530, 401);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label_SelectTable);
+            this.Controls.Add(this.comboBox_SelectTable);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FormEditor";
+            this.Text = "Учет склада";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox comboBox_SelectTable;
+        private System.Windows.Forms.Label label_SelectTable;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
     }
 }
 
--- FormEditor.cs
+++ FormEditor.cs
@@ -7,14 +7,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace WarehouseAccounting
 {
     public partial class FormEditor : Form
     {
+        List<String> Tablenames = new List<String>();
+
+
         public FormEditor()
         {
             InitializeComponent();
+
+            
+            MySqlConnectionStringBuilder mySqlConnectionStringBuilder = new MySqlConnectionStringBuilder();
+
+            mySqlConnectionStringBuilder.Server = "localhost";
+            mySqlConnectionStringBuilder.Port = 3306;
+            mySqlConnectionStringBuilder.Database = "warehouse";
+            mySqlConnectionStringBuilder.UserID = "root";
+            mySqlConnectionStringBuilder.Password = "";
+            MySqlConnection mySqlConnection = new MySqlConnection(mySqlConnectionStringBuilder.ToString());
+
+            
+            try
+            {
+                mySqlConnection.Open();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+
+            MySqlCommand mySqlCommand = new MySqlCommand("SHOW TABLES");
+
+            mySqlCommand.Connection = mySqlConnection;
+
+            MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter();
+
+            MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+
+            while (mySqlDataReader.Read())
+            {
+                Tablenames.Add(mySqlDataReader.GetString(0));
+            }
+
+
+            comboBox_SelectTable.Items.AddRange(Tablenames.ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var formSettings = new FormSettings();
+            formSettings.Show();            
         }
     }
 }
